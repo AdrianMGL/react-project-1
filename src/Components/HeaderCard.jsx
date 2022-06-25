2f08a4dad37708b4aa9b5812cef5a814f6c63113
Adrian MGL <adrienguzmanlopez@gmail.com>
2022-06-25 00:59:30 -0500

    problem logo

--- src/Components/HeaderCard.jsx
+++ src/Components/HeaderCard.jsx
@@ -2,7 +2,7 @@ const HeaderCard = () => {
   return (
     <div className="card__title">
       <h2 className="">Academlo</h2>
-      <img className="bx-tada" src="./../logo.svg" alt="logo" />
+      <img className="bx-tada" src="./logo.svg" alt="logo" />
     </div>
   );
 };
