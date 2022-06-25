cc3a791576756741ed0bff8af0138729ed374415
Adrian MGL <adrienguzmanlopez@gmail.com>
2022-06-25 00:57:51 -0500

    Upps logo

--- src/Components/HeaderCard.jsx
+++ src/Components/HeaderCard.jsx
@@ -2,7 +2,7 @@ const HeaderCard = () => {
   return (
     <div className="card__title">
       <h2 className="">Academlo</h2>
-      <img className="bx-tada" src="./../src/logo.svg" alt="Academlo" />
+      <img className="bx-tada" src="./../logo.svg" alt="logo" />
     </div>
   );
 };
